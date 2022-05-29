3f3bdcf779517a4f021a20db5fd3555e1bb3d5ac
Naol Dame <naododa5566@gmail.com>
2022-05-30 00:15:17 +0300

    defining operational function

--- Calculator/components/screen.js
+++ Calculator/components/screen.js
@@ -1,16 +1,56 @@
-import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Vibration} from 'react-native';
-import {useState} from 'react';
-import { Entypo } from '@expo/vector-icons';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Vibration,
+} from "react-native";
+import { useState } from "react";
+import { Entypo } from "@expo/vector-icons";
 
-function App(){
-    const [darkMode, setDarkMode] = useState(false);
-    const [currentNumber, setCurrentNumber] = useState('');
-    const [lastNumber, setLastNumber] = useState('');
-  
-    const buttons = ['C', 'DEL', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
-  
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [currentNumber, setCurrentNumber] = useState("");
+  const [lastNumber, setLastNumber] = useState("");
 
+  const buttons = [
+    "C",
+    "DEL",
+    "/",
+    7,
+    8,
+    9,
+    "*",
+    4,
+    5,
+    6,
+    "-",
+    1,
+    2,
+    3,
+    "+",
+    0,
+    ".",
+    "=",
+  ];
+  function calculator() {
+    let lastArr = currentNumber[currentNumber.length - 1];
 
+    if (
+      lastArr === "/" ||
+      lastArr === "*" ||
+      lastArr === "-" ||
+      lastArr === "+" ||
+      lastArr === "."
+    ) {
+      setCurrentNumber(currentNumber);
+      return;
+    } else {
+      let result = eval(currentNumber).toString();
+      setCurrentNumber(result);
+      return;
+    }
+  }
 }
-export default App
+export default App;
